85607b554eeb5e84b4f408da5f6094c4d3b9168c
Manikandan Selvam <manikandan.selvam@concertidc.com>
2023-01-23 03:20:32 -0600

    Add queue from and to.

--- src/messages.js
+++ src/messages.js
@@ -4,30 +4,39 @@ const delay = 1000;
 
 module.exports = async () => {
   try {
-    await amqp.assertQueue('on-contact-alert', 'contact', 'contact.alert');
-    await amqp.consume('on-contact-alert', async ({ data: contact }) => {
-      try {
-        console.log('contacts alert created...', contact)
-        return contact;
-      } catch (e) {
-        return e;
-      }
-    }, { maxRetries, delay });
+    await amqp.assertQueue("on-contact-alert", "contact", "contact.alert");
+    await amqp.consume(
+      "on-contact-alert",
+      async ({ data: contact }) => {
+        try {
+          console.log("contacts alert created...", contact);
+          return contact;
+        } catch (e) {
+          return e;
+        }
+      },
+      { maxRetries, delay }
+    );
     await amqp.assertQueue("on-contact-create", "contact", "contacts.created");
     await amqp.consume(
       "on-contact-create",
       async ({ data: contact }) => {
         try {
+          const { from, to } = contact;
           let success = [];
           success.push = contact;
           console.log("Contact accessed...", contact);
-          for (let i=(60*1000); i<=(60*60*1000); i= i + (60*1000)) {
+          for (
+            let i = from * 60 * 1000;
+            i <= to * 60 * 1000;
+            i = i + 60 * 1000
+          ) {
             await amqp.delayedPublish(
-                "contact",
-                "contact.alert",
-                { data: contact },
-                { delay: i }
-              );
+              "contact",
+              "contact.alert",
+              { data: contact },
+              { delay: i }
+            );
           }
           return success;
         } catch (e) {
